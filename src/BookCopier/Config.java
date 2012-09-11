c1181eee61302a30dd3bf8bdd1b1fb8ddad38b3d
Richard Kuba <rikub.cz@gmail.com>
2012-09-11 20:36:32 +0200

    GugaFile used for config load

--- src/BookCopier/Config.java
+++ src/BookCopier/Config.java
@@ -1,13 +1,5 @@
 package BookCopier;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Config 
 {
 	public Config()
@@ -17,54 +9,34 @@ public class Config
 	
 	public void loadConfig()
 	{
-		try 
+		GugaFile file = new GugaFile(filePath, GugaFile.READ_MODE);
+		String line;
+		String[] splittedLine;
+		while ((line = file.ReadLine()) != null)
 		{
-			FileInputStream fRead = new FileInputStream(filePath);
-			DataInputStream inStream = new DataInputStream(fRead);
-			BufferedReader bReader = new BufferedReader(new InputStreamReader(inStream));		
-			String line;
-			String[] splittedLine;
-			try {
-				while ((line = bReader.readLine()) != null)
-				{
-					if(line.startsWith("//"))
-					{
-						continue;
-					}
-					splittedLine = line.split(":");
-					if (splittedLine[0].matches("Only OPs"))
-					{
-						if(splittedLine[1].matches("true"))
-						{
-							this.OPsOnly = true;
-						}
-						
-						else if(splittedLine[1].matches("false"))
-						{
-							this.OPsOnly = false;
-						}
-					}
-					else if(splittedLine[0].matches("Allowed Players"))
-					{
-						this.allowedPlayers = splittedLine[1].split(",");
-					}
-				}
-				bReader.close();
-				inStream.close();
-				fRead.close();
-				
-			} 
-			catch(IOException e) 
+			if(line.startsWith("//"))
 			{
-				e.printStackTrace();		
-			}				
-		} 
-		catch(FileNotFoundException e) 
-		{
-			e.printStackTrace();
+				continue;
+			}
+			splittedLine = line.split(":");
+			if (splittedLine[0].matches("Only OPs"))
+			{
+				if(splittedLine[1].matches("true"))
+				{
+					this.OPsOnly = true;
+				}
+						
+				else if(splittedLine[1].matches("false"))
+				{
+					this.OPsOnly = false;
+				}
+			}
+			else if(splittedLine[0].matches("Allowed Players"))
+			{
+					this.allowedPlayers = splittedLine[1].split(",");
+			}
 		}
 	}
-	
 	public boolean getOPsOnly()
 	{
 		return this.OPsOnly;
